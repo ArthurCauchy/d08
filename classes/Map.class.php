4fb3b7c6ae03915c3537b3e4ddfe912f45fa0a99
Arthur CAUCHY <acauchy@e1r8p14.42.fr>
2018-10-11 17:45:55 +0200

    added map movments

--- classes/Map.class.php
+++ classes/Map.class.php
@@ -41,18 +41,69 @@ class Map {
         $this->_grid[$y][$x] = $entity;
     }
 
-    public function moveEntity($entity, $direction) {
+    public function placeEntity($x1, $y1, $x2, $y2, $entity) {
+        $filling = FALSE;
         for ($i = 0; $i < $this->_ySize; $i++) {
             for ($j = 0; $j < $this->_xSize; $j++) {
-                if ($this->_grid[$i][$j] instanceof $entity) {
-                    if ($j > 1 && $direction === "left") {
-                        if ($this->_grid[$i][$j - 1] === NULL) {
-                            $this->_grid[$i][$j - 1] = $entity;
-                            $this->_grid[$i][$j] = NULL;
+                if ($i == $y1 && $j == $x1)
+                    $filling = TRUE;
+                else if ($i == $y2 && $j == $x2)
+                    return;
+                if ($filling === TRUE)
+                    $this->_grid[$j][$i] = $entity;
+            }
+        }
+    }
+
+    public function moveEntity($entity, $direction) {
+        if ($direction === "left" || $direction === "up") {
+            for ($i = 0; $i < $this->_ySize; $i++) {
+                for ($j = 0; $j < $this->_xSize; $j++) {
+                    if ($this->_grid[$i][$j] instanceof $entity) {
+                        if ($j > 0 && $direction === "left") {
+                            if ($this->_grid[$i][$j - 1] === NULL) {
+                                $this->_grid[$i][$j - 1] = $entity;
+                                $this->_grid[$i][$j] = NULL;
+                            }
+                            else
+                                return ($this->_grid[$i][$j - 1]);
+                        }
+                        if ($i > 0 && $direction === "up") {
+                            if ($this->_grid[$i - 1][$j] === NULL) {
+                                $this->_grid[$i - 1][$j] = $entity;
+                                $this->_grid[$i][$j] = NULL;
+                            }
+                            else
+                                return ($this->_grid[$i - 1][$j]);
                         }
                     }
                 }
             }
         }
+        else if ($direction === "right" || $direction === "down") {
+            for ($i = $this->_ySize; $i > 0; $i--) {
+                for ($j = $this->_xSize; $j > 0; $j--) {
+                    if ($this->_grid[$i][$j] instanceof $entity) {
+                        if ($j > 0 && $direction === "right") {
+                            if ($this->_grid[$i][$j + 1] === NULL) {
+                                $this->_grid[$i][$j + 1] = $entity;
+                                $this->_grid[$i][$j] = NULL;
+                            }
+                            else
+                                return ($this->_grid[$i][$j + 1]);
+                        }
+                        if ($i > 0 && $direction === "down") {
+                            if ($this->_grid[$i + 1][$j] === NULL) {
+                                $this->_grid[$i + 1][$j] = $entity;
+                                $this->_grid[$i][$j] = NULL;
+                            }
+                            else
+                                return ($this->_grid[$i + 1][$j]);
+                        }
+                    }
+                }
+            }
+        }
+        return (NULL);
     }
 }
